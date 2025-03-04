8f067c3ebe8bca0144dd417aa59de8cb705fea22
remifaro <faroremi1@gmail.com>
2025-03-04 10:43:38 -0300

    feat: Primeira tentativa de publicação no Railway

---
+++ src/main/java/me/dio/controller/UserController.java
@@ -0,0 +1,45 @@
+package me.dio.controller;
+
+import me.dio.domain.model.User;
+import me.dio.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /*
+    Precisamos da camada de Service porque o controller vai chama-la, pois é ela que tem as
+    regras de negócio.Aqui estamos acessando a interface, não a implementação. É uma técnica de
+    encapsulamento pra não expor mais do que o necessário.
+    */
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        var user =  userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User userToCreate) {
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+}
+
+
